2e1f374a54e9f00e891cda80086907b714b80aee
maher ben abdesslam <ba.maher94@gmail.com>
2020-04-14 10:50:30 +0100

    change name

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
   //mongoURI: 'mongodb://admin:admin123@ds041228.mlab.com:41228/player-database',
-  mongoURI: 'mongodb+srv://MaherBen:MaherBen@cluster0-untz6.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://MaherBen:MaherBen@cluster0-untz6.mongodb.net/test?retryWrites=true&w=majority',
 
   secretOrKey: 'theMostSecretPhraseEver123!@#'
 };
