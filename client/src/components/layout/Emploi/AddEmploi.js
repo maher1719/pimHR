8ec49477ac0c20fe13875e57803eab903c3f8464
maher ben abdesslam <ba.maher94@gmail.com>
2020-04-19 08:12:59 +0100

    improve code

--- client/src/components/layout/Emploi/AddEmploi.js
+++ client/src/components/layout/Emploi/AddEmploi.js
@@ -1,71 +1,72 @@
-// Helper styles for demo
-
-import {DisplayFormikState} from "./helperFormik";
-
 import React from "react";
-import {Field, Formik} from 'formik';
+import {useSelector} from "react-redux";
+import BigCalendar from "../Calendar/BigCalendar";
+import {Field, Formik} from "formik";
 import * as Yup from "yup";
+import {DisplayFormikState} from "../helperFormik";
 
-const SearchPage = () => (
-    <div className="content-wrapper">
-        {/* Content Header (Page header) */}
-        <section className="content-header">
-            <div className="container-fluid">
-                <div className="row mb-2">
-                    <div className="col-sm-6">
-                        <h1>Blank Page</h1>
-                    </div>
-                    <div className="col-sm-6">
-                        <ol className="breadcrumb float-sm-right">
-                            <li className="breadcrumb-item">
-                                <a href="#">Home</a>
-                            </li>
-                            <li className="breadcrumb-item active">
-                                Blank Page
-                            </li>
-                        </ol>
+const AddEmploi = () => {
+    const user = useSelector(state => state.auth.user);
+
+    return (
+        <div className="content-wrapper">
+            {/* Content Header (Page header) */}
+            <section className="content-header">
+                <div className="container-fluid">
+                    <div className="row mb-2">
+                        <div className="col-sm-6">
+                            <h1>Blank Page</h1>
+                        </div>
+                        <div className="col-sm-6">
+                            <ol className="breadcrumb float-sm-right">
+                                <li className="breadcrumb-item">
+                                    <a href="#">Home</a>
+                                </li>
+                                <li className="breadcrumb-item active">
+                                    Blank Page
+                                </li>
+                            </ol>
+                        </div>
                     </div>
                 </div>
-            </div>
-            {/* /.container-fluid */}
-        </section>
-        {/* Main content */}
-        <section className="content">
-            {/* Default box */}
-            <div className="card">
-                <div className="card-header">
-                    <h3 className="card-title">Title</h3>
-                    <div className="card-tools">
-                        <button
-                            type="button"
-                            className="btn btn-tool"
-                            data-card-widget="collapse"
-                            data-toggle="tooltip"
-                            title="Collapse"
-                        >
-                            <i className="fas fa-minus"/>
-                        </button>
-                        <button
-                            type="button"
-                            className="btn btn-tool"
-                            data-card-widget="remove"
-                            data-toggle="tooltip"
-                            title="Remove"
-                        >
-                            <i className="fas fa-times"/>
-                        </button>
+                {/* /.container-fluid */}
+            </section>
+            {/* Main content */}
+            <section className="content">
+                {/* Default box */}
+                <div className="card">
+                    <div className="card-header">
+                        <h3 className="card-title">Title</h3>
+                        <div className="card-tools">
+                            <button
+                                type="button"
+                                className="btn btn-tool"
+                                data-card-widget="collapse"
+                                data-toggle="tooltip"
+                                title="Collapse"
+                            >
+                                <i className="fas fa-minus"/>
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-tool"
+                                data-card-widget="remove"
+                                data-toggle="tooltip"
+                                title="Remove"
+                            >
+                                <i className="fas fa-times"/>
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div className="card-body">
+                    <div className="card-body">
 
-                    <div className="app">
 
                         <Formik
                             initialValues={{email: ""}}
                             onSubmit={async values => {
                                 await new Promise(resolve => setTimeout(resolve, 500));
                                 alert(JSON.stringify(values, null, 2));
-
+                                console.log()
                             }}
                             validationSchema={Yup.object().shape({
                                 email: Yup.string()
@@ -151,19 +152,16 @@ const SearchPage = () => (
                                 );
                             }}
                         </Formik>
-
-
                     </div>
+                    {/* /.card-body */}
+                    <div className="card-footer">Footer</div>
+                    {/* /.card-footer*/}
                 </div>
-                {/* /.card-body */}
-                <div className="card-footer">Footer</div>
-                {/* /.card-footer*/}
-            </div>
-            {/* /.card */}
-        </section>
-        {/* /.content */}
-    </div>
+                {/* /.card */}
+            </section>
+            {/* /.content */}
+        </div>
+    );
 
-);
-
-export default SearchPage;
+};
+export default AddEmploi;
